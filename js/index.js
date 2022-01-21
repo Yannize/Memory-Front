012be729bc362dfb805340f99631ffe5993058d5
Yannize <yann.hirlemann@gmail.com>
2022-01-21 16:12:35 +0100

    try to connect to backend 1.3

--- js/index.js
+++ js/index.js
@@ -233,9 +233,7 @@ const app = {
     app.listenerInit();
 
     try {
-      const result = await fetch(
-        'https://memory-back.herokuapp.com/get-scores'
-      );
+      const result = await fetch('https://memory-back.herokuapp.com/');
       const scores = await result.json();
       if (scores.length > 0) {
         modals.modalScoreBoard
