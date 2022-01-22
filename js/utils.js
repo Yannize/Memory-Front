de4928795c6bdc444f74d200fecb0c6f689c3a21
Yannize <yann.hirlemann@gmail.com>
2022-01-22 12:21:02 +0100

    sort method update

--- js/utils.js
+++ js/utils.js
@@ -10,7 +10,7 @@ const utils = {
       // on deverse 2x les cartes dans un tableau
       [...cards, ...cards]
         // on trie ce tableau aléatoirement
-        .sort(() => Math.random() - 0.49)
+        .sort(() => Math.random() - 0.5)
         // on map dans ce tableau pour ajouter la propriété id à chaque éléments du tableau
         .map((card, i) => ({ ...card, id: i }))
     );
